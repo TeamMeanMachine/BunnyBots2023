e738aad44956469b66cf3ad0196e87f185eba89b
TMMShared <camasinfiniteresource@gmail.com>
2023-12-11 21:11:38 -0800

    pixy getLowestBall (untested)

--- src/main/kotlin/org/team2471/bunnybots2023/Pixy.kt
+++ src/main/kotlin/org/team2471/bunnybots2023/Pixy.kt
@@ -5,6 +5,7 @@ import io.github.pseudoresonance.pixy2api.Pixy2CCC
 import io.github.pseudoresonance.pixy2api.links.SPILink
 
 import edu.wpi.first.networktables.NetworkTableInstance
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block
 import org.team2471.frc.lib.framework.Subsystem
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -14,31 +15,55 @@ import org.team2471.frc.lib.coroutines.periodic
 object Pixy : Subsystem("Pixy") {
 
     val table = NetworkTableInstance.getDefault().getTable("Pixy")
+    val fpsEntry = table.getEntry("Pixy FPS")
 
-    lateinit var pixy : Pixy2
+    lateinit var pixy: Pixy2
+
+    const val maxBlockCount : Int = 20
+    const val screenHeight : Int = 255
+    const val screenWidth : Int = 255
+
+
 
     init {
-
-    }
-
-    override fun preEnable() {
         initializePixy()
     }
 
     override suspend fun default() {
         periodic {
             if (pixy != null) {
-                println("pixy FPS: ${ pixy.fps }")
+                val lowest = lowestBall()
+                println("x = ${lowest?.x} y = ${lowest?.y}" )
+                fpsEntry.setDouble(pixy.fps.toDouble())
             }
         }
     }
-
     fun initializePixy() {
         pixy = Pixy2.createInstance(SPILink())
         pixy.init()
         pixy.setLamp(0.toByte(), 0.toByte())
     }
+
+    fun lowestBall() : Block? { // returns x and y pos of lowest ball (closest ball) in pixels
+        try {
+//            val count: Int = pixy.ccc.getBlocks(false, (Pixy2CCC.CCC_SIG1.toInt()), maxBlockCount)
+//            if (count == 0) {
+//                return null
+//            }
+
+            var maxBlock : Block? = null
+            val blocks: ArrayList<Pixy2CCC.Block> = pixy.ccc.blockCache
+            maxBlock = blocks.maxByOrNull {it.y}
+
+
+            return maxBlock // higher y coord = lower on screen so reverse it
+        } catch (ex:java.lang.Exception) {
+            println("Pixy 2 exception ${ex.toString()}")
+            return null
+        }
+    }
 }
+
 
 
 
